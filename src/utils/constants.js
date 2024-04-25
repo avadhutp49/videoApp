cfdafe9473ef267b1bc9e89995f8fd70fdfa1afe
avadhutp49 <avadhutp49@gmail.com>
2024-04-25 23:24:57 +0530

    multiple updates

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,7 +7,7 @@ import HtmlIcon from '@mui/icons-material/Html';
 import CssIcon from '@mui/icons-material/Css';
 import SelfImprovementOutlinedIcon from '@mui/icons-material/SelfImprovementOutlined';
 
-export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+export const logo = 'https://github.com/avadhutp49/videoApp/blob/main/src/components/logo.png?raw=true';
 
 export const categories = [
   { name: 'Trending in Tech', icon: <HomeIcon />, },
